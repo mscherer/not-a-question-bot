b6c84b205c7b21e684bd07634ca1dbbee00f8bee
Michael Scherer <misc@redhat.com>
2020-08-21 12:02:20 +0200

    Rewire everything without using a wrapper script
    Since the config is regular python, we can directly get the information from
    thoses variables using os.getenv and python code, which is easier to groke.

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM fedora:32
 RUN dnf install -y python3-pip && dnf clean all
 # use 18.0.0 until https://github.com/jaraco/irc/pull/175 is merged
 RUN pip install errbot irc==18.0.0
-RUN curl https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py > /etc/config.py
-COPY ./errbot.sh /usr/local/bin/
-RUN mkdir -p /usr/local/share/plugins/HeyGuys/
-COPY heyguys.py heyguys.plug /usr/local/share/plugins/HeyGuys/
-CMD ["/usr/local/bin/errbot.sh"]
+
+RUN curl https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py > /etc/config.py.base
+COPY config.py /etc/config.py.custom
+RUN cat /etc/config.py.base /etc/config.py.custom > /etc/config.py
+
+RUN mkdir -p /usr/local/share/plugins/
+COPY HeyGuys/ /usr/local/share/plugins/
+CMD ["/usr/local/bin/errbot", "-c", "/etc/config.py"]
--- config.py
+++ config.py
@@ -1,20 +1,21 @@
-#!/bin/sh
-echo "
+import os
+# this file is added at the end of the default file, so contain
+# only difference from the default
+
 BACKEND = 'IRC'
 BOT_IDENTITY = {
-    'nickname' : '${NICK:-tariste}',
+    'nickname' : os.getenv('NICK', default='tariste'),
     # 'username' : 'err-chatbot',    # optional, defaults to nickname if omitted
     # 'password' : None,             # optional
-    'server' : '${SERVER:-chat.freenode.net}',
-    'port': ${PORT:-6667},                  # optional
+    'server' : os.getenv('SERVER', default='chat.freenode.net'),
+    # 'port': 6667,                  # optional
     # 'ssl': False,                  # optional
     # 'ipv6': False,                 # optional
     # 'nickserv_password': None,     # optional
 }
-CHATROOM_PRESENCE = ('#test-misc-bot',) 
+CHATROOM_PRESENCE = os.getenv('CHANNELS',default='#test-misc-bot').split(';') 
 BOT_DATA_DIR = '/tmp'
 BOT_ADMINS = ('misc',)
 BOT_LOG_FILE = BOT_DATA_DIR + '/bot.log'
 BOT_EXTRA_PLUGIN_DIR = '/usr/local/share/plugins/'
-" >> /etc/config.py
-errbot -c /etc/config.py
+
